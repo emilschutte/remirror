2d30c1134ab7dc760f4997dd80f3c7b44f30e5c3
Ifiok Jr <ifiokotung@gmail.com>
2020-07-26 17:41:07 +0100

    feat(core): deprecate `@remirror/extension-base-keymap`
    Move all keymap functionality to `KeymapExtension` from
    `@remirror/core`. Remove all references to `@remirror/extension-base-keymap`.

--- packages/@remirror/preset-core/src/core-preset.ts
+++ packages/@remirror/preset-core/src/core-preset.ts
@@ -3,7 +3,6 @@ import {
   AnyCombinedUnion,
   CustomHandlerKeyList,
   DefaultPresetOptions,
-  ExtensionPriority,
   getLazyArray,
   GetStaticAndDynamic,
   isEmptyObject,
@@ -11,7 +10,6 @@ import {
   Preset,
   RemirrorManager,
 } from '@remirror/core';
-import { BaseKeymapExtension, BaseKeymapOptions } from '@remirror/extension-base-keymap';
 import { DocExtension, DocOptions } from '@remirror/extension-doc';
 import { HistoryExtension, HistoryOptions } from '@remirror/extension-history';
 import { ParagraphExtension, ParagraphOptions } from '@remirror/extension-paragraph';
@@ -22,8 +20,7 @@ import { TextExtension } from '@remirror/extension-text';
  * The static settings for the core preset.
  */
 export interface CorePresetOptions
-  extends BaseKeymapOptions,
-    DocOptions,
+  extends DocOptions,
     ParagraphOptions,
     PositionerOptions,
     HistoryOptions {}
@@ -31,15 +28,11 @@ export interface CorePresetOptions
 export class CorePreset extends Preset<CorePresetOptions> {
   static defaultOptions: DefaultPresetOptions<CorePresetOptions> = {
     ...DocExtension.defaultOptions,
-    ...BaseKeymapExtension.defaultOptions,
     ...ParagraphExtension.defaultOptions,
     ...HistoryExtension.defaultOptions,
   };
 
-  static customHandlerKeys: CustomHandlerKeyList<CorePresetOptions> = [
-    'keymap',
-    'positionerHandler',
-  ];
+  static customHandlerKeys: CustomHandlerKeyList<CorePresetOptions> = ['positionerHandler'];
 
   get name() {
     return 'core' as const;
@@ -51,28 +44,14 @@ export class CorePreset extends Preset<CorePresetOptions> {
   protected onSetOptions(parameter: OnSetOptionsParameter<CorePresetOptions>) {
     const { pickChanged } = parameter;
     const changedParagraphOptions = pickChanged(['indentAttribute', 'indentLevels']);
-    const changedBaseKeymapOptions = pickChanged([
-      'defaultBindingMethod',
-      'selectParentNodeOnEscape',
-      'excludeBaseKeymap',
-      'undoInputRuleOnBackspace',
-    ]);
 
     if (!isEmptyObject(changedParagraphOptions)) {
       this.getExtension(ParagraphExtension).setOptions(changedParagraphOptions);
     }
-
-    if (!isEmptyObject(changedBaseKeymapOptions)) {
-      this.getExtension(BaseKeymapExtension).setOptions(changedBaseKeymapOptions);
-    }
   }
 
-  protected onAddCustomHandler: AddCustomHandler<CorePresetOptions> = (parameter) => {
-    const { keymap, positionerHandler } = parameter;
-
-    if (keymap) {
-      return this.getExtension(BaseKeymapExtension).addCustomHandler('keymap', keymap);
-    }
+  protected onAddCustomHandler: AddCustomHandler<CorePresetOptions> = (handlers) => {
+    const { positionerHandler } = handlers;
 
     if (positionerHandler) {
       return this.getExtension(PositionerExtension).addCustomHandler(
@@ -87,12 +66,8 @@ export class CorePreset extends Preset<CorePresetOptions> {
   createExtensions() {
     const {
       content,
-      defaultBindingMethod,
-      excludeBaseKeymap,
       indentAttribute,
       indentLevels,
-      selectParentNodeOnEscape,
-      undoInputRuleOnBackspace,
       depth,
       getDispatch,
       getState,
@@ -105,13 +80,6 @@ export class CorePreset extends Preset<CorePresetOptions> {
       new TextExtension(),
       new ParagraphExtension({ indentAttribute, indentLevels }),
       new PositionerExtension(),
-      new BaseKeymapExtension({
-        defaultBindingMethod,
-        excludeBaseKeymap,
-        selectParentNodeOnEscape,
-        undoInputRuleOnBackspace,
-        priority: ExtensionPriority.Low,
-      }),
     ];
   }
 }
